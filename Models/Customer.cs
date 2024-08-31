40e7f954567dc9d830467bee91602a0f2dd4a881
safinghoghabori <safinghoghabori65@gmail.com>
2024-08-31 09:56:29 +0530

    add customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,11 @@
+namespace RestaurantReservationSystemAPI.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
